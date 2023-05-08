81b67d30ede617431bae3b5c461b582e43de88ca
Rocio <rochisoutullo12@gmail.com>
2023-05-08 20:51:15 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// VARIABLES 
+// declaración / nombre / asignación de valor / datos
+
+// OPERACIONES MATEMÁTICAS
+let n1=1
+let n2=2
+let resultadoSuma= n1+n2
+console.log(resultadoSuma)
+
+let nombre = 'Luna'
+let apellido = 'Rodriguez'
+let raza = 'Labrador'
+let espacioBlanco = '  '
+let usuario = nombre + espacioBlanco + apellido + espacioBlanco + raza
+console.log (usuario)
