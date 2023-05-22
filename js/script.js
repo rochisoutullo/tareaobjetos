ca9669cb3fabd37e97e8761a21358842b8935b79
Rocio <rochisoutullo12@gmail.com>
2023-05-21 23:20:57 -0300

    objetos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-// VARIABLES 
-// declaración / nombre / asignación de valor / datos
-
-// OPERACIONES MATEMÁTICAS
-let n1=1
-let n2=2
-let resultadoSuma= n1+n2
-console.log(resultadoSuma)
-
-let nombre = 'Luna'
-let apellido = 'Rodriguez'
-let raza = 'Labrador'
-let espacioBlanco = '  '
-let usuario = nombre + espacioBlanco + apellido + espacioBlanco + raza
-console.log (usuario)
+//OBJECTO
+class Persona {
+    constructor(nombre,apellido) {
+      this.nombre = Rocío;
+      this.apellido = Soutullo;
+    }
+  
+    mostrarMensaje() {
+      alert('¡Hola, soy' + this.nombre + this.apellido);
+    }
+  }
+  
+  //objeto a partir de la clase constructora
+  const asesora1 = new Persona('Rocio', 'Soutullo')
+  
+  // Llamar al método personalizado para mostrar el mensaje
+  asesora1.mostrarMensaje();
